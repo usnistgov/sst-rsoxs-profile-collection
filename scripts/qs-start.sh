089bc99c97e34c0f581c3bd2d0e1eb06c8f52c4f
SST Operator <xf07id1@xf07id-ws24.nsls2.bnl.local>
2026-01-20 14:40:47 -0500

    Everything working on pixi

--- scripts/qs-start.sh
+++ scripts/qs-start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 set -e
 set -o xtrace
-start-re-manager --redis-addr redis:6379 --zmq-publish-console ON --use-ipython-kernel ON --ipython-kernel-ip auto --startup-profile collection
+start-re-manager --zmq-publish-console ON --use-ipython-kernel ON --ipython-kernel-ip auto --startup-profile collection
